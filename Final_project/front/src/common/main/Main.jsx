2fc5988d67f8687219a0cabf2df3a3471375c098
Makar90 <Makarlancer@gmail.com>
2023-03-09 13:36:31 +0200

    React6 Strapi

---
+++ Final_project/front/src/common/main/Main.jsx
@@ -0,0 +1,11 @@
+import './index.css'
+
+export default function Main() {
+    return(
+        <main>
+            <div className="container">
+                <h1>Main</h1>
+            </div>
+        </main>
+    );
+}
--- Reacr4/front/src/App.js
+++ Reacr4/front/src/App.js
@@ -1,7 +1,44 @@
+import React from 'react';
+import { useState } from 'react';
+
+
 function App() {
+    let operator1 = React.createRef();
+    let operator2 = React.createRef();
+    let operationOperator = React.createRef();
+    let resault = React.createRef();
+
+    function Calculate() {
+        if (isNaN(operator1.current.value) || isNaN(operator2.current.value)) {
+            console.log('error');
+            return 'Error';
+        }
+        switch (operationOperator.current.value) {
+            case '+': return +operator1.current.value + +operator2.current.value;
+            case '-': return +operator1.current.value - +operator2.current.value;
+            case '*': return +operator1.current.value * +operator2.current.value;
+            case '/': return +operator1.current.value / +operator2.current.value;
+            default: return "Error";
+        }
+    }
+
+    function ShowResault() {
+        resault.current.value = Calculate();
+    }
     return (
         <div className="App">
-            test
+
+            <input ref={operator1} type='text' defaultValue='оператор 1' />
+            <select ref={operationOperator}>
+                <option value='+'>+</option>
+                <option value='-'>-</option>
+                <option value='*'>*</option>
+                <option value='/'>/</option>
+            </select>
+            <input ref={operator2} type='text' defaultValue='оператор 2' />
+            <button onClick={ShowResault}>CALCULATE</button>
+            <input ref={resault} type='text' defaultValue='результат' />
+
         </div>
     );
 }
